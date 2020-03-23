3d7e863a5cb7d4c9e893d1468ac13769271fd3df
Veronan <veronan@pm.me>
2020-03-23 22:59:52 +0800

    Add basic zip-base64 func

--- main.go
+++ main.go
@@ -9,17 +9,17 @@ import (
 	"github.com/1set/gut/ystring"
 )
 
-var (
-	method     string
-	fileName   string
-	rawContent string
-)
-
 const (
 	EMPTY         = ""
 	METHOD_ENCODE = "encode"
 	METHOD_DECODE = "decode"
 	FLAG_POSTFIX  = " (shorthand)"
+)
+
+var (
+	method     string
+	fileName   string
+	rawContent string
 )
 
 func main() {
@@ -31,24 +31,41 @@ func main() {
 	flag.StringVar(&rawContent, "r", EMPTY, "raw input content"+FLAG_POSTFIX)
 	flag.Parse()
 
-	if method != METHOD_ENCODE && method != METHOD_DECODE {
-		fmt.Printf("invalid method: %s\n", method)
-		flag.Usage()
-		os.Exit(1)
-	}
-
+	var content string
 	if ystring.IsNotBlank(fileName) {
 		if b, err := ioutil.ReadFile(fileName); err != nil {
 			fmt.Printf("fail to read file %q: %v\n", fileName, err)
 			os.Exit(3)
 		} else {
-			rawContent = string(b)
+			content = string(b)
 		}
-	} else if ystring.IsBlank(rawContent) {
+	} else if ystring.IsNotBlank(rawContent) {
+		content = rawContent
+	} else {
 		fmt.Println("got no input arguments, specify 'file' or 'raw'")
 		flag.Usage()
 		os.Exit(2)
 	}
 
 	fmt.Printf("content: {%s}\n", rawContent)
+	switch method {
+	case METHOD_ENCODE:
+		if encodeResult, err := base64Deflate.Encode([]byte(content)); err != nil {
+			fmt.Printf("fail to encode: %v\n", err)
+			os.Exit(4)
+		} else {
+			fmt.Println(encodeResult)
+		}
+	case METHOD_DECODE:
+		if decodeResult, err := base64Deflate.Decode(content); err != nil {
+			fmt.Printf("fail to decode: %v\n", err)
+			os.Exit(5)
+		} else {
+			fmt.Println(string(decodeResult))
+		}
+	default:
+		fmt.Printf("invalid method: %s\n", method)
+		flag.Usage()
+		os.Exit(1)
+	}
 }
